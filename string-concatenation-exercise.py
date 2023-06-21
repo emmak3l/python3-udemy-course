4025c53a26c9f25dab464e565d08de77e40aa862
Emma <emma.a.kellam@gmail.com>
2023-06-21 15:26:30 -0400

    Deleted exercises from main folder

--- string-concatenation-exercise.py
+++
@@ -1,8 +0,0 @@
-#Set the variable called greeting  to some greeting, e.g. "hello".
-greeting = "Howdy"
-
-#Set the variable called name  to some name, e.g. "Heisenberg".
-name = "Woody"
-
-#Then set the variable called greet_name  so that it concatenates greeting , name , and a space " " between them.
-greet_name = greeting + " " + name
