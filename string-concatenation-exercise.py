f3bef0c6a0966ce9d3a6f29b82a16fa922729c85
Emma <emma.a.kellam@gmail.com>
2023-06-21 15:21:04 -0400

    Completed String Concatenation Exercise

---
+++ string-concatenation-exercise.py
@@ -0,0 +1,8 @@
+#Set the variable called greeting  to some greeting, e.g. "hello".
+greeting = "Howdy"
+
+#Set the variable called name  to some name, e.g. "Heisenberg".
+name = "Woody"
+
+#Then set the variable called greet_name  so that it concatenates greeting , name , and a space " " between them.
+greet_name = greeting + " " + name
