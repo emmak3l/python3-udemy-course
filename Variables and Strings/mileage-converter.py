0aec181b5036c8e4ab5fb143cfba50e25387edbb
Emma <emma.a.kellam@gmail.com>
2023-06-21 16:00:21 -0400

    Completed Mileage Converter exercise

---
+++ Variables and Strings/mileage-converter.py
@@ -0,0 +1,5 @@
+print("How many kilometers do you want to convert to miles?")
+kms = input()
+miles = float(kms)/1.60934
+miles = round(miles, 2)
+print(f"{kms} kilometers is equal to {miles} miles.")
