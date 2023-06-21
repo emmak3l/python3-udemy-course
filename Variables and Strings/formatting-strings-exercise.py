06ea4ebf57f6f7bacc2d0fc7ecbfc1673247439c
Emma <emma.a.kellam@gmail.com>
2023-06-21 15:34:25 -0400

    Completed Formatting Strings Exercise

---
+++ Variables and Strings/formatting-strings-exercise.py
@@ -0,0 +1,8 @@
+#Set the variable called first  to your first name.
+first = "Emma"
+
+#Set the variable called last  to your last name.
+last = "K"
+
+#Then set the variable called formatted  to a new string that interpolates both values (from the first and last variables) using f-strings or the .format()  method.  Make sure you follow this exact pattern for the new string that you store to the formatted variable (pay attention to capital/lowercase letters, spaces, commas, etc)
+formatted = f"First Name: {first}, Last Name: {last}"
