391e942f3e8ec60d5e5365af548d8efcbf80f0d2
Emma <emma.a.kellam@gmail.com>
2023-06-22 11:56:09 -0400

    Completed Bouncer coding example

--- Booleans and Conditional Logic/bouncer-example.py
+++ Booleans and Conditional Logic/bouncer-example.py
@@ -3,12 +3,12 @@ age = input("How old are you?: ")
 
 if age:
 	age = int(age)
-	if age >= 18 and age < 21:
-		# 18-21 wristband
-		print("You can enter, but you need a wristband!")
-	elif age >= 21:
+	if age >= 21:
 		# 21+ drink, normal entry
 		print("You can enter and can drink!")
+	elif age >= 18:
+		# 18-21 wristband
+		print("You can enter, but you need a wristband!")
 	else:
 		# too young sorry
 		print("You can't come in little one, sorry!")
