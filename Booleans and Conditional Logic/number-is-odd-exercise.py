c4e01a33a3c7d4147bc37d40c30b7143147fb372
Emma <emma.a.kellam@gmail.com>
2023-06-22 10:54:16 -0400

    Completed Number is Odd coding exercise

---
+++ Booleans and Conditional Logic/number-is-odd-exercise.py
@@ -0,0 +1,18 @@
+#You will be provided with a random number in a variable called num .
+#Use a conditional statement to check if the number is odd. If num  is odd, print "odd". Otherwise print "even". 
+#Hint: use modulus %  to figure out if the number is odd!
+
+# NO TOUCHING ======================================
+from random import randint
+num = randint(1, 1000) #picks random number from 1-1000
+# NO TOUCHING ======================================
+
+
+
+# YOUR CODE GOES HERE vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+if num % 2 == 1:
+    print("odd")
+else:
+    print("even")
+
+# YOUR CODE GOES HERE ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
