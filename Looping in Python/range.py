49145c41227acde5c4253b4bb9ae4eb34aacac2c
Emma <emma.a.kellam@gmail.com>
2023-06-26 16:04:23 -0400

    Basic range examples from lecture

---
+++ Looping in Python/range.py
@@ -0,0 +1,11 @@
+# Print numbers from 0 to 10 (doesn't include 10 as it's exclusive)
+# for num in range(10):
+# 	print(num)
+
+# Print even numbers from 10 to 20
+# for num in range(10,20,2):
+# 	print(num)
+
+# Print multiples of 5 from 0 to 100
+for num in range(0,100,5):
+	print(num)
