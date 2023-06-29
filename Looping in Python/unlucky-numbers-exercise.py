0b241405ac323ef7a7eb9fff2a7ba37286bbe1df
Emma <emma.a.kellam@gmail.com>
2023-06-29 15:07:07 -0400

    Added optimized solution for unlucky numbers exercise

--- Looping in Python/unlucky-numbers-exercise.py
+++ Looping in Python/unlucky-numbers-exercise.py
@@ -1,7 +1,18 @@
+# My first solution with multiple print statements
+# for x in range(1,21):
+# 	if x == 4 or x == 13:
+# 		print(f"{x} is UNLUCKY!")
+# 	elif x % 2 == 1:
+# 		print(f"{x} is odd")
+# 	else:
+# 		print(f"{x} is even")
+
+# Optimized solution below with only one print statement
 for x in range(1,21):
 	if x == 4 or x == 13:
-		print(f"{x} is UNLUCKY!")
+		state = "UNLUCKY!"
 	elif x % 2 == 1:
-		print(f"{x} is odd")
+		state = "odd"
 	else:
-		print(f"{x} is even")
+		state = "even"
+	print(f"{x} is {state}")
