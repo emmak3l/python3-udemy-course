d792d3a72d5e27ee6a728f587413820e89b860ca
Emma <emma.a.kellam@gmail.com>
2023-08-14 13:19:14 -0400

    Competed stop copying me exercise

---
+++ Looping in Python/stop-copying-me-exercise.py
@@ -0,0 +1,8 @@
+# Instructions: Repeat everything the user says until the user says 'stop copying me'
+
+user_input = ""
+print("Hey how's it going? ")
+while user_input != "stop copying me":	
+	user_input = input()
+	print(user_input)
+print("UGH FINE YOU WIN")
