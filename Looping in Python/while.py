3849356b9b8bd7b40ac6568a178f6af1ff68ae78
Emma <emma.a.kellam@gmail.com>
2023-08-14 12:09:21 -0400

    Transcribed instructors while loop examples

---
+++ Looping in Python/while.py
@@ -0,0 +1,13 @@
+# Below is the first while loop example the instructor provided:
+
+# msg = input("what's the secret password? ")
+# while msg != "bananas":
+# 	print("WRONG!")
+# 	msg = input("what's the secret password? ")
+# print("CORRECT!")
+
+# Second while loop example:
+num = 1
+while num < 11:
+	print(num)
+	num += 1
