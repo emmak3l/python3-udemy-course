09d6eb02ca8a6d820433a13fcb8fa11af6c2569d
Emma <emma.a.kellam@gmail.com>
2023-08-18 11:41:26 -0400

    Completed lists basic exercise

---
+++ Lists/lists-basic-exercise.py
@@ -0,0 +1,13 @@
+# Lists Basic Exercise
+
+# Now that we've learned about lists, let's get some practice.
+# See the problem statements in the comments below:
+
+# Initially create an empty list called instructors
+instructors = []
+# Add the following strings to the instructors list 
+    # "Colt"
+    # "Blue"
+    # "Lisa"
+instructors.extend(['Colt', 'Blue', 'Lisa'])
+# Run the tests to make sure you've done this correctly!
