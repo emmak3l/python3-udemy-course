33114032aa6e8184f72969b1a52044f57231b711
Emma <emma.a.kellam@gmail.com>
2023-08-18 10:29:37 -0400

    Completed creating lists exercise

---
+++ Lists/creating_lists_exercise.py
@@ -0,0 +1,16 @@
+# Creating Lists Exercise
+# It's time to create your own lists. Define 2 separate lists:
+#	- First, define a list called my_stuff. It must be at least 4 elements long.
+#	  The data is completely up to you, but it must contain at least 1 string and 1 float
+#	- Next, define a list called nums.
+#	  It should be a list that contains all the numbers from 1-99 (including 99)
+#	  Don't type this out manually! Use what we learned at the end of the previous video!
+
+# Define my_stuff
+my_stuff = ['phone', 'computer', 6.7888, True, 1, False]
+
+# Define nums
+nums = list(range(1,100))
+
+print(nums)
+print(my_stuff)
