b2ad939cddf4baba9d0e08256cb74467f5724f87
Emma <emma.a.kellam@gmail.com>
2023-08-20 14:12:04 -0400

    Completed list comp exercise 5

---
+++ List Comprehension/list-exercise-5.py
@@ -0,0 +1,10 @@
+# List Exercise 5
+
+# Using a nested list comprehension and range(), create a variable called answer
+# with the following value - [[0, 1, 2], [0, 1, 2], [0, 1, 2]] . 
+# To break it down...start by using range and a list comp to generate the list [0,1,2].
+# And then repeat that whole thing 3 times in a nested list comp. 
+# It's all a bit tricky to discuss, so maybe it's just best to look at the solution if you get stuck.  
+
+answer = [[num for num in range(0,3)] for num in range(0,3)]
+print(answer)
