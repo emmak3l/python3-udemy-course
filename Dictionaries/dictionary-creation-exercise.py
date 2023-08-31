6f7a9ac106e1187acce618152d01bd0a2c3efc91
Emma <emma.a.kellam@gmail.com>
2023-08-31 10:47:17 -0400

    Completed dictionary creation exercise

---
+++ Dictionaries/dictionary-creation-exercise.py
@@ -0,0 +1,4 @@
+# Dictionary Creation Exercise
+# Create a dictionary called user_info and add at least 3 key value pairs to it
+
+user_info = dict(name="Emma", favAnimal="dogs", favColour="violet")
