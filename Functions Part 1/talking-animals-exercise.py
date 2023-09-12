6a68f62ed23c79b350a83b40f756e5f5a762ac52
Emma <emma.a.kellam@gmail.com>
2023-09-12 15:43:28 -0400

    Completed talking animals exercise

---
+++ Functions Part 1/talking-animals-exercise.py
@@ -0,0 +1,31 @@
+# Default Parameter Exercise - Talking Animals
+
+# Write a function called speak  that accepts a single parameter, animal.  
+
+# If animal is "pig", it should return "oink". 
+# If animal is "duck", it should return "quack".  
+# If animal is "cat", it should return "meow"
+# If animal is "dog", it should return "woof"
+# If animal is anything else, it should return "?"
+# If no animal is specified, it should default to "dog"
+
+
+# speak()  # "woof"
+# speak("pig")  # "oink"
+# speak("duck")  # "quack"
+# speak("cat")  # "meow"
+# speak("dog")  # "woof"
+# speak("banana")  # "?"
+
+# Define speak below:
+def speak(animal="dog"):
+	if animal == "dog":
+		return "woof"
+	elif animal == "pig":
+		return "oink"
+	elif animal == "duck":
+		return "quack"
+	elif animal == "cat":
+		return "meow"
+	else:
+		return "?"
