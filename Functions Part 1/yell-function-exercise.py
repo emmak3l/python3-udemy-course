3308829134504e228cabd34b6cea10ef2859474d
Emma <emma.a.kellam@gmail.com>
2023-09-12 15:14:44 -0400

    Completed yell function exercise

---
+++ Functions Part 1/yell-function-exercise.py
@@ -0,0 +1,11 @@
+# Yell Function Exercise
+# Implement a function yell  which accepts a single string argument.  It should return(not print) an uppercased version of the string with an exclamation point aded at the end.  For example:
+
+# yell("go away")   # "GO AWAY!"
+
+# yell("leave me alone")   # "LEAVE ME ALONE!"
+
+# You do not need to call the function to pass the tests.
+
+def yell(string):
+	return string.upper() + "!"
