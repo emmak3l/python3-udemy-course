16c4e4880b1e3fa840121685d306ac47af2fd264
Emma <emma.a.kellam@gmail.com>
2023-09-27 10:53:55 -0400

    Completed built-in modules exercise

---
+++ Modules/built-in-modules-exercise.py
@@ -0,0 +1,7 @@
+# Built In Modules Exercise
+# It's time to get some practice with built-in modules.  Here's your mission;
+
+# Import the math  module
+import math
+# Use math.sqrt  to find the square root of 15129 and save it to variable called answer
+answer = math.sqrt(15129)
