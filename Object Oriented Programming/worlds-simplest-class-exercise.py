fbe1799ed5a7e11887974f68a1ad12d344dc43ba
Emma <emma.a.kellam@gmail.com>
2023-10-07 16:22:26 -0400

    Completed world's simplest class exercise

---
+++ Object Oriented Programming/worlds-simplest-class-exercise.py
@@ -0,0 +1,17 @@
+# World's Simplest Class Exercise
+# Define a class called Vehicle.
+#  It should be completely empty (just add a pass statement inside)
+#   After the class is defined, create two instances of Vehicle.
+#    Save one to a variable called car  and another to a variable called boat
+
+# naming convention is to name classes in the singular (like User instead of Users) and to use CamelCase
+
+# define the Vehicle class below:
+class Vehicle:
+	pass
+
+# instantiate a new Vehicle and save it to a variable called car:
+car = Vehicle()
+
+# instantiate a new Vehicle and save it to a variable called boat:
+boat = Vehicle()
