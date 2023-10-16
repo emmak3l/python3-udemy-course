a35be805c4275e73aa3c13a6091056a4d89341df
Emma <emma.a.kellam@gmail.com>
2023-10-16 11:44:45 -0400

    Completed get multiples exercise

---
+++ Iterators and Generators/get-multiples-exercise.py
@@ -0,0 +1,23 @@
+# get_multiples
+# Write a function called get_multiples, which accepts a number and a count,
+#  and returns a generator that yields the first count multiples of the number. 
+
+# The default number should be 1, and the default count should be 10.
+
+'''
+evens = get_multiples(2, 3)
+next(evens) # 2
+next(evens) # 4
+next(evens) # 6
+next(evens) # StopIteration
+
+default_multiples = get_multiples()
+list(default_multiples) # [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+'''
+
+def get_multiples(num=1, count=10):
+	original_num = num
+	while count > 0:
+		yield num
+		num += original_num
+		count -= 1
