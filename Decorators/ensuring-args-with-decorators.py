c804c12f68b87b92470053aee0a8808f971de0c7
Emma <emma.a.kellam@gmail.com>
2023-10-17 11:03:01 -0400

    Completed ensure no kwargs with decorators lecture example

---
+++ Decorators/ensuring-args-with-decorators.py
@@ -0,0 +1,18 @@
+# Ensuring args with Decorators
+
+from functools import wraps
+
+def ensure_no_kwargs(fn):
+	@wraps(fn)
+	def wrapper(*args, **kwargs):
+		if kwargs:
+			raise ValueError("No kwargs allowed! Sorry :(")
+		return fn(*args, **kwargs)
+	return wrapper
+
+@ensure_no_kwargs
+def greet(name):
+	print(f"hi there {name}")
+
+greet("Tony") # hi there Tony
+greet(name="Tony") # ValueError: No kwargs allowed! Sorry :(
