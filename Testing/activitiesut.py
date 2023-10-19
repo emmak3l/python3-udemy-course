cb76bc161f19a856c62c3ba9c1d7543c402d1329
Emma <emma.a.kellam@gmail.com>
2023-10-19 15:23:49 -0400

    Updated intro-to-unittest.py file and activitiesut.py file to add the additional assert tests from the Other Types of Assertions lecture

--- Testing/activitiesut.py
+++ Testing/activitiesut.py
@@ -1,4 +1,9 @@
+from random import choice
+
 def eat(food, is_healthy):
+	if not isinstance(is_healthy, bool):
+		raise ValueError("Is healthy must be a boolean")
+		
 	ending = "because YOLO!"
 
 	if is_healthy:
@@ -10,3 +15,10 @@ def nap(num_hours):
 	if num_hours >= 2:
 		return f"Ugh I overslept. I didn't mean to nap for {num_hours} hours!"
 	return f"I'm feeling refreshed after my {num_hours} hour nap"
+
+def is_funny(person):
+	if person is 'tim': return False
+	return True
+
+def laugh():
+	return choice(('lol', 'haha', 'tehehe'))
--- Testing/intro-to-unittest.py
+++ Testing/intro-to-unittest.py
@@ -12,7 +12,7 @@
 # - You can run tests by calling unittest.main()
 
 import unittest
-from activitiesut import eat, nap
+from activitiesut import eat, nap, is_funny, laugh
 
 class ActivityTests(unittest.TestCase):
 	def test_eat_healthy(self):
@@ -29,6 +29,10 @@ class ActivityTests(unittest.TestCase):
 			"I'm eating pizza, because YOLO!"
 
 			)
+	def test_eat_healthy_boolean(self):
+		"""is_healthy must be a bool"""
+		with self.assertRaises(ValueError):
+			eat("pizza", is_healthy='who cares?')
 	def test_short_nap(self):
 		"""short naps should be refreshing"""
 		self.assertEqual(
@@ -41,6 +45,17 @@ class ActivityTests(unittest.TestCase):
 			nap(3),
 			"Ugh I overslept. I didn't mean to nap for 3 hours!"
 			)
+	def test_is_funny_tim(self):
+		self.assertEqual(is_funny("tim"), False) # this is checking for False
+		# self.assertFalse(is_funny("tim"), "tim should not be funny") # this is checking for Falsy values, not just False
+	def test_is_funny_anyone_else(self):
+		"""anyone else but tim should be funny"""
+		self.assertTrue(is_funny("blue"), "blue should be funny")
+		self.assertTrue(is_funny("tammy"), "tammy should be funny")
+		self.assertTrue(is_funny("sven"), "sven should be funny")
+	def test_laugh(self):
+		"""laugh should return one of the values in the tuple ('lol', 'haha', 'tehehe')"""
+		self.assertIn(laugh(), ('lol', 'haha', 'tehehe'))
 		
 
 if __name__ == "__main__":
@@ -50,4 +65,22 @@ if __name__ == "__main__":
 # We can add a docstring(comments in triple quotes) to our test functions to get more info in terminal
 # To see the comments we added, run the file like this:
 # python3 NAME_OF_TEST_FILE.py -v
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Types of Assertions
+# - self.assertEqual(x, y)
+# - self.assertNotEqual(x, y)
+# - self.assertTrue(x)
+# - self.assertFalse(x)
+# - self.assertIsNone(x)
+# - self.assertIsNotNone(x)
+# - self.assertIn(x, y)
+# - self.assertNotIn(x, y)
+# - and more...
+
+
+
+
 
