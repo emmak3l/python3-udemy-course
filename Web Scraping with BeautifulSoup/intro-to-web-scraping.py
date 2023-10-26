e61db456b97902aae5fde5b2c6bcba03c7d0355a
Emma <emma.a.kellam@gmail.com>
2023-10-26 16:03:40 -0400

    Completed intro to webscraping lecture notes

---
+++ Web Scraping with BeautifulSoup/intro-to-web-scraping.py
@@ -0,0 +1,27 @@
+# Web Scraping
+
+# Objectives:
+# - Define what web scraping is and the issues surrounding it
+# - Use the requests and BeautifulSoup modules to parse HTML
+# - Explain some common technical problems with web scraping
+# - Explore other tools that can interact with web pages
+
+
+# What is Web Scraping?
+# - Web scraping involves programmatically grabbing data from a web page
+# - Three Steps: Download, extract data, do something with the data (PROFIT! :P)
+
+
+# Why Scrape?
+# - There's data on a site that you want to store or analyze
+# - You can't get by other means (e.g. an API)
+# - You want to programmatically grab the data (instead of lots of manual copying/pasting)
+
+
+# Is it... ok?
+# - Some websites don't want people scraping them
+# - Best practice: consult the robots.txt file
+# - If making many requests, time them out
+# - If you're too aggressive, your IP can be blocked
+
+
